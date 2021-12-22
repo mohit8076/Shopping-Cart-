edd378e28cdf3d38d261b84496099ee759e3e8ec
Mohit <mynkchauhan1234@gmail.com>
2021-12-22 14:29:29 +0530

    Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
-// Components
+
 import Item from './Item/Item';
 import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
@@ -8,9 +8,9 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
-// Styles
+
 import { Wrapper, StyledButton } from './App.styles';
-// Types
+
 export type CartItemType = {
   id: number;
   category: string;
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -1,7 +1,6 @@
 import Button from '@material-ui/core/Button';
-// Types
+
 import { CartItemType } from '../App';
-// Styles
 import { Wrapper } from './CartItem.styles';
 
 type Props = {
